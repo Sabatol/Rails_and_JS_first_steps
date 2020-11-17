74c201483503962ef563a7101c2333981c1aeafb
Sabatol <u.sacrez@gmail.com>
2020-11-17 19:32:56 +0100

    First commit

---
+++ app/assets/config/manifest.js
@@ -0,0 +1,4 @@
+//= link_tree ../images
+//= link_directory ../stylesheets .css
+// Ligne ajoutée pour aller chercher les fichiers à partir du dossier que l'on vient de rajouter (app/assets)
+//= link_tree ../sounds
---
+++ test/controllers/vinyls_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class VinylsControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get vinyls_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get vinyls_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get vinyls_new_url
+    assert_response :success
+  end
+
+end
